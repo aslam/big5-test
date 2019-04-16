5901ebe468789df4813f2d5931e53acf75cb60f8
Syed Aslam <aslam@hashlearn.com>
2019-04-16 12:56:56 +0530

    add class to convert textual results into hash
    - initializer requires file path
    - to_h method takes no arguments, returns a hash in the specified format

---
+++ big_five_results_text_serializer.rb
@@ -0,0 +1,29 @@
+class BigFiveResultsTextSerializer
+    attr_accessor :file_path
+
+    def initialize (file_path)
+        if !File.exists?(file_path) || !File.readable?(file_path) || !File.file?(file_path)
+            raise "File doesn't seem to be readable at this path."
+        end
+
+        @file_path = file_path
+    end
+
+    def to_h
+        results = { 'NAME' => 'Syed Aslam' }
+
+        if File.zero?(file_path)
+            raise "File doesn't have any content!"
+        end
+
+        File.open(@file_path).readlines.each do |line|
+            scores = line.split(',').each_slice(2).to_a
+            domain = scores.shift
+            results[domain.first] = { "Overall Score" => domain[1].to_i }
+
+            results[domain.first]['Facets'] = Hash[scores]
+        end
+
+        results
+    end
+end
